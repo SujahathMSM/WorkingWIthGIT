73e8f57122777bf2fcbc169a98400a924fbfc5ce
sujahathmsm <sujahathmhmd@gmail.com>
2024-04-27 13:03:02 +0530

    Initial Commit

---
+++ Example.java
@@ -0,0 +1,25 @@
+class Singleton{
+    private static Singleton singleton;
+
+    private Singleton(){
+
+    }
+
+    public static Singleton getInstance(){
+        if (singleton == null){
+            singleton = new Singleton();
+        }
+
+        return singleton;
+    }
+}
+
+class Example{
+    public static void main(String[] args) {
+        Singleton a = Singleton.getInstance();
+        System.out.println(a);
+
+        Singleton b = Singleton.getInstance();
+        System.out.println(b);
+    }
+}
